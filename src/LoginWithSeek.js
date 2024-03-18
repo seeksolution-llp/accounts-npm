17fe084d1996cef9b002bda2fda09bee5516ff1a
Seek Solution LLP <projects@seeksolution.in>
2024-03-18 17:31:07 +0530

    removed some code

--- src/LoginWithSeek.js
+++ src/LoginWithSeek.js
@@ -1,26 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginWithSeek = void 0;
+exports.loginWithSeek = exports.addListener = void 0;
+var listeners = [];
+function addListener(listener) {
+    listeners.push(listener);
+}
+exports.addListener = addListener;
 function loginWithSeek() {
     try {
         console.log("loginWithSeek called");
         var childWindow = window.open('https://auth-seek.vercel.app/signin', '_blank', 'width=300,height=300');
         // Listener to handle messages from the child window
         window.addEventListener('message', function (event) {
+            console.log("event", event);
+            listeners.forEach(function (listener) { return listener(event.data); });
             // Check origin for security purposes
             if (event.origin === 'https://auth-seek.vercel.app/signin') {
                 console.log('Message received from child window:', event.data);
                 // You can perform any action here with the received data
             }
             else {
-                console.warn('Message received from untrusted origin:', event.origin);
+                console.warn('Message received from untrusted origin:', event.origin, event.data);
             }
-            this.setInterval(function () {
-                window.opener.postMessage("Hello from child!", "*");
-            }, 2000);
         });
-        // Send a message to child window
-        childWindow === null || childWindow === void 0 ? void 0 : childWindow.postMessage('Hello from parent!', 'https://auth-seek.vercel.app/signin');
     }
     catch (error) {
         console.log("error", error);
--- src/LoginWithSeek.ts
+++ src/LoginWithSeek.ts
@@ -1,3 +1,11 @@
+
+type ValueChangeListener = (value: any) => void;
+const listeners: ValueChangeListener[] = [];
+
+export function addListener(listener: ValueChangeListener): void {
+    listeners.push(listener);
+}
+
 export function loginWithSeek() {
     try {
         console.log("loginWithSeek called");
@@ -6,20 +14,17 @@ export function loginWithSeek() {
 
         // Listener to handle messages from the child window
         window.addEventListener('message', function (event) {
+            console.log("event", event);
+
+            listeners.forEach(listener => listener(event.data));
             // Check origin for security purposes
             if (event.origin === 'https://auth-seek.vercel.app/signin') {
                 console.log('Message received from child window:', event.data);
                 // You can perform any action here with the received data
             } else {
-                console.warn('Message received from untrusted origin:', event.origin);
+                console.warn('Message received from untrusted origin:', event.origin, event.data);
             }
-            this.setInterval(() => {
-                window.opener.postMessage("Hello from child!", "*");
-            }, 2000)
         });
-
-        // Send a message to child window
-        childWindow?.postMessage('Hello from parent!', 'https://auth-seek.vercel.app/signin');
 
     } catch (error) {
         console.log("error", error);
